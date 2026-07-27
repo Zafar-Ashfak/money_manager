eeb06b7a4c92f86ba42427f72a5181353c303ba4
Zafar Ashfak <Zafarashfak008@gmail.com>
2026-07-27 23:39:18 +0530

    Implement Spring Security configuration

--- src/main/java/com/springboot/money_manager/config/SecurityConfig.java
+++ src/main/java/com/springboot/money_manager/config/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsPasswordService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -69,9 +68,11 @@ public class SecurityConfig {
 
     @Bean
     public AuthenticationManager authenticationManager() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsPasswordService((UserDetailsPasswordService) appUserDetailsService);
+        DaoAuthenticationProvider authenticationProvider =
+                new DaoAuthenticationProvider(appUserDetailsService);
+
         authenticationProvider.setPasswordEncoder(passwordEncoder());
+
         return new ProviderManager(authenticationProvider);
     }
 }
